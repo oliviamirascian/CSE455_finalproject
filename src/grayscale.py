1617862f8fcc11ef19716f7461fe914bcad23445
hoanganhminh01 <hoanganhminh01@gmail.com>
2022-03-10 01:20:32 -0800

    update too much

---
+++ src/grayscale.py
@@ -0,0 +1,13 @@
+import cv2
+import os
+
+
+src_dir = '../input/animefaces'
+images = os.listdir(src_dir)
+dst_dir = '../input/grayscaled'
+
+for i, img in enumerate(images):
+    img = cv2.imread(f"{src_dir}/{images[i]}")
+    # add gaussian blurring
+    blur = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite(f"{dst_dir}/{images[i]}", blur)
